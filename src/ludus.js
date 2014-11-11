9ff163020b722580d450aa5888fabc837337b347
Tyler O'Brien <obrienty@gmail.com>
2014-11-11 15:20:01 -0500

    Update copyright info in judus.js

--- src/ludus.js
+++ src/ludus.js
@@ -1,8 +1,9 @@
 /*
  * LudusJS
  * https://github.com/TylerOBrien/LudusJS
+ * http://tylerobrien.com
  *
- * Copyright (c) 2012 Tyler O'Brien
+ * Copyright (c) 2014 Tyler O'Brien
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
